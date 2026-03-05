94e1515be4ca157105dbe8563de3ec26680b0dc8
Kyu Lee <kwooler@gmail.com>
2026-03-05 02:21:33 -0800

    Student starter code

--- main.cpp
+++ main.cpp
@@ -9,12 +9,6 @@ int main()
     cin >> num1 >> num2 >> num3;
 
     // TODO: Find the smallest value among three values
-    min = num1;
-    if (num2 < min)
-        min = num2;
-    if (num3 < min)
-        min = num3;
-    // END TODO
 
     // Use the following statements to print output
     cout << "The smallest number is  " << min << endl;
